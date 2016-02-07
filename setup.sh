93e152f8697ef45d0fdad5cc0628941697a40e3f
Kathrin Hanauer <kal@xaikal.org>
2016-02-07 21:49:51 +0100

    Added bashrc with bash-git-prompt.
    Signed-off-by: Kathrin Hanauer <kal@xaikal.org>

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2016 Kathrin Hanauer
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,12 +22,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-DOTFILES=(bash_aliases tmux.conf vimrc)
-
-for f in ${DOTFILES[@]}
-do
-	SOURCE=$(realpath $f)
-	TARGET="${HOME}/.$f"
+create_symlink() {
+	SOURCE=$1
+	TARGET=$2
 	echo -ne "Linking ${SOURCE} as ${TARGET}... "
 	if [ -f ${SOURCE} ] && [ ! -f ${TARGET} ]
 	then
@@ -41,5 +38,42 @@ do
 	else
 		echo "already exists."
 	fi
+}
+
+
+DOTFILES=(bash_aliases bashrc tmux.conf vimrc)
+
+for f in ${DOTFILES[@]}
+do
+	SOURCE=$(realpath $f)
+	TARGET="${HOME}/.$f"
+	create_symlink ${SOURCE} ${TARGET}
 done
+
+GITPROMPT="bash-git-prompt"
+CURDIR=$(pwd)
+
+cd ..
+SOURCE="$(pwd)/${GITPROMPT}"
+TARGET="${HOME}/.${GITPROMPT}"
+ALLOK=1
+
+if [ ! -e ${GITPROMPT} ]
+then
+	echo "Installing ${GITPROMPT}..."
+	git clone https://github.com/magicmonty/bash-git-prompt.git ${GITPROMPT} && ALLOK=0
+elif [ -d ${GITPROMPT} ]
+then
+	echo -e "Updating ${GITPROMPT}..."
+	cd ${GITPROMPT} && git pull && ALLOK=0
+else
+	echo -e "Cannot setup bash-git-prompt."
+fi
+
+if [ ${ALLOK} -eq 0 ]
+then
+	create_symlink ${SOURCE} ${TARGET}
+fi
+
+cd ${CURDIR}
 
